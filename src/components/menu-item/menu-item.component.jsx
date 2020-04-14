027ecc52eecc8d22a37507ae6b295e1d8f59f640
notheera <itsmeankit12feb@gmail.com>
2020-04-14 19:47:19 +0530

    added collection preview $ collection item with styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import './menu-item.styles.scss'
 
-const menuItem = ({ title, imageUrl, size }) => {
+const menuItem = ({ title, imageUrl, size, history }) => {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} >
             <div className='background-image'
-            style={{ backgroundImage: `url(${imageUrl})` }} />
+                style={{ backgroundImage: `url(${imageUrl})` }} />
             <div className='content'>
                 <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOP NOW</span>
@@ -14,4 +15,4 @@ const menuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default menuItem
+export default withRouter(menuItem)
